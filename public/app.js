b99306ed23533ec9248858a1d00a84c466a76940
stellie82 <kims82@gmail.com>
2020-01-11 20:04:35 -0800

    add data for article items and add function to clear page

--- public/app.js
+++ public/app.js
@@ -2,9 +2,9 @@ var refreshArticles = function() {
     $.getJSON("/articles", function(data) {
         console.log(data);
         for (var i = 0; i < data.length; i++) {
-            var card = $("<div>").attr({
-                class: "card"
-            });
+            var card = $("<div article-id='" + data[i]._id + "'>");
+
+            var header = $("<h5>").addClass("card-header");
 
             var title = $("<a>")
                 .attr("href", data[i].link)
@@ -12,25 +12,40 @@ var refreshArticles = function() {
                 .text(data[i].title)
                 .addClass("article-title");
 
-            var imgURL = data[i].image;
-            console.log(imgURL);
-            var image = $("<img>").attr({
-                src: imgURL,
-                class: "article-img",
-                alt: "article-photo"
-            });
-
             var saveBtn = $("<button>")
                 .addClass("btn btn-sm btn-outline-secondary save-button")
                 .text("Save Article");
 
-            card.append(title);
-            card.append(image);
-            card.append(saveBtn);
+            header.append(title);
+            header.append(saveBtn);
+
+            var body = $("<div>")
+                .addClass("card-body");
+
+            var imgURL = data[i].image;
+            var image = $("<img>").attr({
+                src: imgURL,
+                class: "article-img",
+                alt: "article-photo",
+                style: "width: 200px"
+            });
+
+            var summary = $("<p>").text(data[i].summary);
+
+            body.append(image);
+            body.append(summary);
+
+            card.append(header);
+            card.append(body);
             $("#articles-container").append(card);
         };
     });
 };
 
+var clearArticles = function() {
+    $("#articles-container").empty();
+};
+
 // Add event listener for the refresh button
 $("#refresh").on("click", refreshArticles);
+$("#clear").on("click", clearArticles);
